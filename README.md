359a829a536229a7b68559bdd320a10f649e93c2
David <dscherer21@gmail.com>
2019-05-19 01:04:31 -0500

    setup router, switch stations, and initial landingpage setup

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a recreation of my DateNight app, but built with ReactJS.
 
-Languages and Technologies used: HTML5, CSS3, ES6(JavaScript), NodeJS, ReactJS
+Languages and Technologies used: HTML5, CSS3, ES6(JavaScript), NodeJS, ReactJS, ExpressJs(Middleware)
---
+++ client/components/landingpage/landingpage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './landingpage.css';
+import { Container, Row, Col } from 'react-bootstrap';
+//import { Link } from "react-router-dom";
+
+
+const LandingPage = () => {
+    return(
+      <Container>
+          <Row>
+              <Col>
+                <p>Hello!</p>
+              </Col>
+          </Row>
+      </Container>
+    )
+}
+
+
+export default LandingPage;
